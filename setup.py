a3e1fa18ec89f67db2cfe98638e81aa2dc712226
L <66860610+CookingANut@users.noreply.github.com>
2022-06-24 09:49:21 +0800

    add file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='daemontool',
+        version='2.0',
+        author='daemon_huang',
+        install_requires=[],
+        python_requires='>=3',
+        py_modules=['daemontool'],
+        )
+
+        
