4e7ac6c473217068e5263eb9da29999eaf797a4a
L <66860610+CookingANut@users.noreply.github.com>
2022-07-19 13:58:13 +0800

    update

--- __AUTOMAKE.py
+++ __AUTOMAKE.py
@@ -1,12 +1,26 @@
-# Description:scirpt for auto make one file library
+# Description: scirpt for auto make one file python library
 # Author: Daemon Huang
 # Date: 2022/07/19
 
-
 import os
+import sys
 from daemontool import VERSION
-# os.system('pip install wheel')
-# os.system('python -m pip install --upgrade pip')
+# 'pip install wheel' if you find command abnormal
+# 'python -m pip install --upgrade pip' update pip(optional)
+
+command = os.system
+join = os.path.join
+
+if sys.platform.startswith('win'):
+    rm = 'del /f /s /q '
+    rm_rf = 'rd /s /q '
+    mv = 'move '
+    install = 'pip3 install '
+else:
+    rm = 'rm '
+    rm_rf = 'rm -rf '
+    mv = 'mv '
+    install = 'pip install '
 
 
 class LibMaker():
@@ -15,9 +29,9 @@ class LibMaker():
     """
 
     def __init__(self):
-        self.setup_path = os.path.join(os.getcwd(), 'setup.py')
-        self.dist_path = os.path.join(os.getcwd(), 'dist')
-        self.egg_path = os.path.join(os.getcwd(), 'daemontool.egg-info')
+        self.setup_path = join(os.getcwd(), 'setup.py')
+        self.dist_path = join(os.getcwd(), 'dist')
+        self.egg_path = join(os.getcwd(), 'daemontool.egg-info')
 
     def create_setup(self):
         """create temp setup.py"""
@@ -39,24 +53,23 @@ class LibMaker():
     def remove_old(self):
         """remove old dist folder"""
         # in windows need to use "{path}" to include the path, not '{path}'
-        os.system(f'rd /s /q "{self.dist_path}"')
-        os.system(f'del /f /s /q "./*.tar.gz"')
+        command(f'{rm}"./*.tar.gz"')
 
     def make_lib(self):
         """make lib"""
-        os.system(f'python "{self.setup_path}" sdist')
-        os.system(f'del /f /s /q "{self.setup_path}"')
-        os.system(f'rd /s /q "{self.egg_path}"')
+        command(f'python "{self.setup_path}" sdist')
+        command(f'{rm}"{self.setup_path}"') # remove temp setup.py
+        command(f'{rm_rf}"{self.egg_path}"') # remove egg-info folder
         
     def if_install(self):
         """ask if need to install lib"""
-        lib_path = os.path.join(self.dist_path, os.listdir(self.dist_path)[0])
+        lib_path = join(self.dist_path, os.listdir(self.dist_path)[0])
         lib_name = os.listdir(self.dist_path)[0]
-        os.system(f'move "{lib_path}" .')
-        os.system(f'rd /s /q "{self.dist_path}"')
+        command(f'{mv}"{lib_path}" .') # move lib to workspace
+        command(f'{rm_rf}"{self.dist_path}"') # remove dist folder
         need_install = input('Do you need install it now? y/n ')
         if need_install == 'y':
-            os.system(f'pip3 install "./{lib_name}"')
+            command(f'{install}"./{lib_name}"')
 
     def auto_build(self):
         """auto run sequence"""
@@ -68,12 +81,12 @@ class LibMaker():
     @classmethod
     def installation(cls):
         """"for installation used only"""
-        dist_path = os.path.join(os.getcwd(), 'dist')
-        lib_path = os.path.join(dist_path, os.listdir(dist_path)[0])
-        os.system(f'pip3 install "{lib_path}"')
+        lib_name = [x for x in os.listdir('.') if 'tar.gz' in x ][0]
+        command(f'{install}"./{lib_name}"')
 
 if __name__ == '__main__':
     LibMaker().auto_build()
+    # LibMaker.installation()
 
 
 
